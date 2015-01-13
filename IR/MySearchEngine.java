a1445b5c033976a6a361c8317a6eb479fe75ef72
Jwala Mohith Girisha <jwalamohithgsjce@gmail.com>
2015-01-13 02:12:03 -0800

    Project

---
+++ IR/MySearchEngine.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MySearchEngine{
+	
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+	
+		System.out.println("This is my search engine");
+	
+		System.out.println("Loading.....\n\n");
+	
+		System.out.println("Part1: Text Processing\n-----------------------------------------------------------------\n");
+
+		TextProcessing.fnBeginTextProcessing();
+	
+	}
+	
+}
---
+++ IR/TextProcessing.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.File;
+ 
+ 
+public class TextProcessing{
+
+	public static void fnBeginTextProcessing(){
+		
+		Scanner in = new Scanner(System.in);
+		
+		List tokenList = new ArrayList();
+		
+		String filePath;
+		System.out.println("Enter the file path:");
+		
+		filePath = in.nextLine();
+		File file = new File(filePath);
+	
+		if(file.exists() && !file.isDirectory())
+			tokenList = tokenizeFile(filePath);
+		else{
+			System.out.println("File not found");
+			return;
+		}			
+			
+		System.out.println("Tokens: " + tokenList);
+			
+	}
+	
+	
+	public static List tokenizeFile(String FileName){
+		
+		BufferedReader brSample = null;
+		List tokenList = new ArrayList();
+		
+		try {
+ 
+			String strSample;
+			brSample = new BufferedReader(new FileReader(FileName));
+ 
+			while ((strSample = brSample.readLine()) != null) {
+				
+				System.out.println(strSample);
+		
+				for(String words: strSample.split("[^a-zA-Z0-9]+")){
+					tokenList.add(words);
+				}
+		
+			}
+ 
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			try {
+				if (brSample != null)
+					brSample.close();
+					
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	
+		return tokenList;
+	}
+	
+}
