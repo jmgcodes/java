77bc5a17bfb8b29c6862aebc4633754ef0a6be20
Jwala Mohith Girisha <jwalamohithgsjce@gmail.com>
2015-01-16 11:46:47 -0800

    Text Processing

--- IR/TextProcessing.java
+++ IR/TextProcessing.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.StringTokenizer;
+import java.util.Map.Entry;
+
 /*
  * Author: Jwala Mohith Girisha
  * 
@@ -18,8 +20,9 @@ public class TextProcessing{
 		Scanner in = new Scanner(System.in);
 		
 		List<String> tokenList = new ArrayList();
-		Map<String,Integer> tokenPair = new TreeMap<String, Integer>();
-		
+		Map<String,Integer> tokenPair = new HashMap<String, Integer>();
+		Map<String,Integer> twoGramPair = new HashMap<String, Integer>();
+
 		
 		//PartA
 		String filePath;
@@ -41,11 +44,18 @@ public class TextProcessing{
 		//PartB
 		
 		tokenPair = computeWordFrequencies(tokenList);
-		print(tokenPair);
+		printToken(tokenPair);
+		
+		
+		//PartC
+		
+		twoGramPair = computeTwoGramFrequencies(tokenList);
+		printTwoGram(twoGramPair);
+		
 			
 	}
 	
-	
+	//Part A
 	public static List<String> tokenizeFile(String FileName){
 		
 		BufferedReader brSample = null;
@@ -84,13 +94,14 @@ public class TextProcessing{
 	
 	public static void print(List<String> tokenList){
 		
-		//System.out.println("Tokens: " + tokenList);
+		System.out.println("Tokens: " + tokenList);
 		
 	}
 	
+	//Part B
 	public static Map<String, Integer> computeWordFrequencies(List<String> tokenList){
 		
-		Map<String,Integer> tokenPairTemp = new TreeMap<String, Integer>();
+		Map<String,Integer> tokenPairTemp = new HashMap<String, Integer>();
 		
 		for (String token : tokenList) {
 			Integer count = tokenPairTemp.get(token);
@@ -102,18 +113,68 @@ public class TextProcessing{
 	
 	}
 	
-	public static void print(Map<String, Integer> tokenPair){
+	public static void printToken(Map<String, Integer> tokenPair){
 		
 		System.out.println("\nTokens: " + tokenPair.size());
 		
-		//System.out.println("\n\tToken\t\tFrequency\n\t-----\t\t---------\n");
+		System.out.println("\n\tFrequency - Token\n\t---------   -----\n");
 		
-		for(String token : tokenPair.keySet()){
+		Set<Entry<String, Integer>> tokenPairSet = tokenPair.entrySet();
+        List<Entry<String, Integer>> tokenPairList = new ArrayList<Entry<String, Integer>>(tokenPairSet);
+        Collections.sort( tokenPairList, new Comparator<Map.Entry<String, Integer>>()
+        {
+            public int compare( Map.Entry<String, Integer> mapEntry1, Map.Entry<String, Integer> mapEntry2 )
+            {
+                return (mapEntry2.getValue()).compareTo( mapEntry1.getValue() );
+            }
+        } );
+        
+        for(Map.Entry<String, Integer> mapEntry:tokenPairList){
+            System.out.println("\t" + mapEntry.getValue() + "\t" + mapEntry.getKey());
+        }
+						
+	}
+	
+	//Part C
+	
+	public static Map<String, Integer> computeTwoGramFrequencies(List<String> tokenList){
+		
+		Map<String,Integer> twoGramPairTemp = new HashMap<String, Integer>();
+		
+		String previous = "";
+		for (String token : tokenList) {
+			String tokenTemp = (previous == "") ? token: previous + " " + token;
+			previous = token;
+			Integer count = twoGramPairTemp.get(tokenTemp);
+			count = (count == null) ? 1 : ++count;
+			twoGramPairTemp.put(tokenTemp, count);
 			
-			//System.out.println("\t" + token + "\t\t" + tokenPair.get(token));
-
 		}
 		
+		return twoGramPairTemp;
+	
+	}
+	
+	public static void printTwoGram(Map<String, Integer> twoGramPair){
+		
+		System.out.println("\nTwo Grams: " + twoGramPair.size());
+		
+		System.out.println("\n\tFrequency - Two Grams\n\t---------   ---------\n");
+		
+		Set<Entry<String, Integer>> twoGramPairSet = twoGramPair.entrySet();
+        List<Entry<String, Integer>> twoGramPairList = new ArrayList<Entry<String, Integer>>(twoGramPairSet);
+        Collections.sort( twoGramPairList, new Comparator<Map.Entry<String, Integer>>()
+        {
+            public int compare( Map.Entry<String, Integer> mapEntry1, Map.Entry<String, Integer> mapEntry2 )
+            {
+                return (mapEntry2.getValue()).compareTo( mapEntry1.getValue() );
+            }
+        } );
+        
+        for(Map.Entry<String, Integer> mapEntry: twoGramPairList){
+            System.out.println("\t" + mapEntry.getValue() + "\t" + mapEntry.getKey());
+        }
+						
 	}
 	
 }
